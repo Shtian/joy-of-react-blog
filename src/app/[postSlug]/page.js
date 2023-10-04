f024a4403d14bd218e56eca7089b4ef6ee7a1cd7
Stian Haga <hagastian@gmail.com>
2023-10-04 14:05:05 +0200

    exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,15 +5,29 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { BLOG_TITLE } from "@/constants";
+
+const loadCachedBlogPost = React.cache(async (slug) => {
+  const post = await loadBlogPost(slug);
+  return post;
+});
+
+export async function generateMetadata({ params }) {
+  const post = await loadCachedBlogPost(params.postSlug);
+
+  return {
+    title: `${post.frontmatter.title} • ${BLOG_TITLE}`,
+    description: post.frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
-  const post = await loadBlogPost(params.postSlug);
-  console.log(post);
+  const post = await loadCachedBlogPost(params.postSlug);
   return (
     <article className={styles.wrapper}>
       <BlogHero
         title={post.frontmatter.title}
-        publishedOn={new Date(post.frontmatter.publishedOn)}
+        publishedOn={post.frontmatter.publishedOn}
       />
       <div className={styles.page}>
         <MDXRemote source={post.content} />
